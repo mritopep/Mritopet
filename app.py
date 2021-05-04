1fc8568a6e43d566eb5a9e7ecfeba3a2e9727258
ashia <ashiajoseph@gmail.com>
2021-05-04 18:16:20 +0530

    upload completed

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, request, send_file
+from flask import Flask, render_template, request, send_file,redirect
 from mri2pet import Mri2Pet
-from os import path
+from os import path,mkdir
 from flask_ngrok import run_with_ngrok
+from werkzeug.utils import secure_filename
 
 app_root = path.dirname(path.abspath(__file__))
 
@@ -10,7 +11,7 @@ app = Flask(__name__,
             static_folder='./static',
             template_folder='./templates')
 
-# run_with_ngrok(app)
+#run_with_ngrok(app)
 
 model = Mri2Pet()
 print(model,flush=True)
@@ -34,8 +35,52 @@ def test():
 
 @app.route("/download", methods=['GET', 'POST'])
 def download():
-    download = path.join(app_root, 'output/nii/output.nii.gz')
+    download = path.join(app_root, 'output/nii/pet.nii.gz')
     return send_file(download, as_attachment=True)
+
+
+def create_folders():
+    input_folder = path.join(app_root, 'input')
+    if not path.exists(input_folder):
+        mkdir(input_folder)
+        mkdir(path.join(input_folder,"nii"))
+        mkdir(path.join(input_folder,"img"))
+
+    output_folder = path.join(app_root, 'output')
+    if not path.exists(output_folder):
+        mkdir(output_folder)
+        mkdir(path.join(output_folder,"nii"))
+        mkdir(path.join(output_folder,"img"))
+
+def supported_file(filename):
+    if not "." in filename:
+        return false
+    ext=filename.rsplit(".",1)[1]
+    if ext=="nii":
+        return True
+    else:
+        return False
+
+@app.route("/upload", methods=['GET', 'POST'])
+def upload():
+    global model
+    if request.method == 'POST':
+        if request.files:
+            f = request.files['mri_file']
+            if not supported_file(f.filename):
+                print("File not supported")
+                return redirect(request.url)
+            else:
+                create_folders()
+                filename=secure_filename(f.filename)
+                f.save(path.join(app_root,'input','nii',filename))
+                print("uploaded")
+                #model.next(path.join(app_root,'input','nii',filename))
+                print('Pet saved')
+
+            return redirect(request.url)
+            #'file uploaded successfully'
+    return render_template("index.html")
 
 if __name__ == '__main__':
     app.run()
--- mri2pet.py
+++ mri2pet.py
@@ -1,10 +1,16 @@
-
 from keras.models import load_model
 from PIL import Image
 import nibabel as nib
 from numpy import load, zeros, copy, arange, eye, uint8
 from os import path
 import cv2
+from os import listdir
+from os import system as run
+import numpy as np
+from numpy import savez_compressed
+from med2img.med2img import convert
+import shutil
+
 THIS_FOLDER = path.dirname(path.abspath(__file__))
 
 def load_test():
@@ -31,6 +37,42 @@ def img_to_nii(img):
     # data = arange(4*4*3).reshape(4,4,3)
     image = nib.Nifti1Image(img, affine=eye(4))
     return image
+    
+def pre_process_mri(image, gamma_correction=False):
+
+    if gamma_correction:
+        gamma = 0.15
+        invGamma = 1.0 / gamma
+
+        table = np.array([((i / 255.0) ** invGamma) * 255
+                          for i in np.arange(0, 256)]).astype("uint8")
+
+        image = cv2.LUT(image, table).astype(np.uint8)
+
+    return image
+
+
+# file name should be having .nii
+def save_to_npz(file, des = ""):
+    #run("mkdir images")
+    #run("med2image -i " + file + " -o /images/slice")
+    convert(file,"./input/img/img")
+    folder = "./input/img"
+    files = sorted(listdir(folder))
+    print(len(files))
+    list = []
+    for i in range(len(files)):
+        data = cv2.imread(folder + files[i])
+        data = pre_process_mri(data, gamma_correction=True)
+        list.append(data)
+    list = np.asarray(list)
+    
+    if des != "" : des += "/"
+    filename = des + 'images.npz'
+    savez_compressed(filename, list)
+    
+    #shutil.rmtree("images") #run("rm -rf images")
+    return list
 
 class Mri2Pet:
 
@@ -43,13 +85,20 @@ class Mri2Pet:
         output_img = self.predict(inp_images)
         output_nii = img_to_nii(output_img)
         nib.save(output_nii, path.join(THIS_FOLDER, 'output/nii/output.nii.gz')) 
+    
+    def next(self,file):
+        inp_images = save_to_npz(file)
+        output_img = self.predict(inp_images)
+        output_nii = img_to_nii(output_img)
+        nib.save(output_nii, path.join(THIS_FOLDER, 'output/nii/pet.nii.gz'))
+
 
     def predict(self, inp_images):
         fake_images = zeros(inp_images.shape)
         for i in range(len(inp_images)):
         	fake_images[i] = self.model.predict(inp_images[i:i+1])
         predicted_imgs = copy(fake_images)
-        predicted_data = path.join(THIS_FOLDER, 'output/img')
+        predicted_data = path.join(THIS_FOLDER, 'output','img')
         print(len(predicted_imgs))
         for i in range(len(inp_images)):
             img = predicted_imgs[i]

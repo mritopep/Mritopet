d3b1298c2999355d422f6f58f34d2e8d46ff7787
Antony J Malakkkaran <antonyjm462@gmail.com>
2021-05-09 13:02:02 +0530

    change made

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ app = Flask(__name__,
             static_folder='./static',
             template_folder='./templates')
 
-# run_with_ngrok(app)
+run_with_ngrok(app)
 
 model = Mri2Pet()
 print(bcolors.BOLD, model, bcolors.ENDC, flush=True)

6e08544779ce157f209aba8343c0c1645fe13909
Antony J Malakkkaran <antonyjm462@gmail.com>
2021-05-09 12:51:16 +0530

    preprocess scripts added

---
+++ shell_scripts/img_rgr.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+source /home/miniconda/etc/profile.d/conda.sh &> ./logs/null.txt
+conda init bash &> ./logs/null.txt
+conda activate simple_itk &> ./logs/null.txt
+soft/image_rgr.py $1 $2 $3 &&  echo 'passed' > ./logs/$4 || echo 'failed' > ./logs/$4
